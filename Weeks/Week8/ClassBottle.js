284a1922e6b05c10ff056f9f30570573ba7b9c23
SagaraKyzen <sagararizky1002@gmail.com>
2025-04-26 10:59:30 +0700

    Add files via upload

---
+++ Weeks/Week8/ClassBottle.js
@@ -0,0 +1,27 @@
+class Botol{
+    string Merek;
+    string Warna;
+    int CapasitasMl;
+
+    void bukabotol(){
+        // logika membuka botol
+    }
+
+    void plintirbotol(){
+        // logika plintir botol untuk dibuang
+
+    }
+
+    void buangbotol(){
+        // logika membuang botol pada tempatnya
+    }
+}
+
+Botol Minumsaya = new Botol();
+Minumsaya.Merek = "LeMineral";
+Minumsaya.Warna = "Bening";
+Minumsaya.CapasitasMl = 300;
+
+Minumsaya.bukabotol();
+Minumsaya.plintirbotol();
+Minumsaya.buangbotol();
